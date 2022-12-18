77d680cc29f164df26aaf29dc20bd6730a5431ad
karimdaw1991 <karimdaw91@gmail.com>
2022-12-18 22:52:38 +0100

    add wiki file and play with file

--- wiki.go
+++ wiki.go
@@ -13,7 +13,24 @@ type Page struct {
 
 // This is a method named save that takes as its receiver
 // p, a pointer to Page . It takes no parameters, and returns a value of type error
-func (p *Page) save() error {
-    filename := p.Title + ".txt"
-    return os.WriteFile(filename, p.Body, 0600)
+func (page *Page) save() error {
+    filename := page.Title + ".txt"
+    return os.WriteFile(filename, page.Body, 0600)
+}
+
+// method loads a page if it exists, if not, returns error
+func loadPage(title string) (*Page, error) {
+    filename := title + ".txt"
+    body, err := os.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+    return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+    page1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+    page1.save()
+    // page2, _ := loadPage("TestPage")
+    // fmt.Println(string(page2.Body))
 }
